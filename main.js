20f219b5a0c2bf3cce883215c55110a0d566f10d
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-03-09 19:11:47 -0500

    feat(Taller): :sparkles: par e impar

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
- let nota1 = parseFloat( prompt("Ingrese su nota1:"));
- let nota2 = parseFloat( prompt("Ingrese su nota2:"));
- let nota3 = parseFloat ( prompt("Ingrese su nota3:"));
- //promediar
-let resultado = (nota1+nota2+nota3) / 3 ;
-//se hace el filtro de la condicion 
-if (resultado <= 3.9){
-    console.log("Estudie")
-}else{
-    console.log("becado")
-}
+ let numero = parseFloat( prompt("Ingrese su nota1:"));
+
+ const result= (numero % 2 == 0)  ? "es par y ": "es impar y ";
+ const result2= (numero >=10 ) ? "es mayor que 10":" es menor que 10";
+ console.log(result+result2 ) 
