d7e3ca740ff05b80a3f87aba2965590566cee1aa
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-03-09 21:21:21 -0500

    feat(Taller): :sparkles: Nombresedad

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-import {calcularvoltaje} from './voltage.js';
+let personas = [];
 
-const resistencia = prompt ("introduce el valor de resistencia ")
-const intensidad = prompt("introduce el valor de intensidad")
+for(let i = 1; i <= 3; i++){
+    let nombre = prompt("Ingrese su nombre" +i);
+    let edad = prompt("ingrese su edad "+i);
+    personas.push({nombre: nombre, edad: edad});
 
-const voltaje = calcularvoltaje(resistencia,intensidad);
-console.log("El valor del voltaje es ", voltaje);
+}
+
+let edadMaxima = 0; 
+let indiceMaximo = 0; 
+
+for (let i=0; i < personas.length; i++){
+    if(personas[i].edad > edadMaxima){
+        edadMaxima = personas[i].edad;
+        indiceMaximo= i;
+
+    }
+}
+
+console.log("La persona con la mayor edad es" + personas[indiceMaximo].nombre)
