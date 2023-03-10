411dff2d27114ba6e0c090c9171a2d038044c2d3
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-03-09 19:26:29 -0500

    feat(Taller): :sparkles: par e impar

--- main.js
+++ main.js
@@ -2,4 +2,4 @@
 
  const result= (numero % 2 == 0)  ? "es par y ": "es impar y ";
  const result2= (numero >=10 ) ? "es mayor que 10":" es menor que 10";
- console.log(result+result2 ) 
+ console.log(result+result2 )
