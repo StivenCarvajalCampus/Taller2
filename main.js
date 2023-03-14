d3b52992355986ab3b60ac9e12e25b4e79b1077b
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-03-13 19:50:51 -0500

    feat(Taller): :sparkles: ejercicio9

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-let numero1 = parseInt( prompt("ingrese un numero"));
-let numero2 = parseInt (prompt("ingrese numero 2"));
-
-if (numero1>numero2){
-    let suma= numero1 + numero2;
-    let diferencia = numero1-numero2;
-    console.log("el resultado es " + suma);
-    console.log("la diferencia es " + diferencia);
-}else{
-    let producto = numero1 * numero2;
-    let division = numero1 / numero2;
-    console.log("el producto es " + producto);
-    console.log("la division es " + division);
-
+//Ejercicio 9
+/* 
+N atletas han pasado a finales en salto triple en los juegos
+olímpicos femenino de 2022. Diseñe un programa que pida por
+teclado los nombres de cada atleta finalista y a su vez, sus
+marcas del salto en metros. Informar el nombre de la atleta
+campeona que se quede con la medalla de oro y si rompió
+récord, reportar el pago que será de 500 millones. El récord
+esta en 15,50 metros.*/
+let competidores=[]
+let lista='';
+let record='';
+let mensaje='';
+function atleta(nombre,marca){
+    this.nombre=nombre;
+    this.marca=marca;
 }
+do{
+    let nombre = prompt('Digite su nombre: ');
+    let marca = prompt('Digite su marca: ');
+    let atle = new atleta(nombre, marca);
+    competidores.push(atle);
+}while(confirm("¿Desea ingresar otro competidor?"))
+record=competidores[0].marca;
+mensaje=`El competidor con mayor recor es ${competidores[0].nombre} con un recor de ${record} metros`;
+
+for (let i=0; i<competidores.length; i++) {
+    lista +=`Nombre: ${competidores[i].nombre} Marca: ${competidores[i].marca} metros \n`;
+
+    if(competidores[i].marca>record){
+        record=competidores[i].marca;
+        mensaje=`El competidor con mayor record es ${competidores[i].nombre} con un recor de ${record} metros`;
+    }
+}
+
+console.log(lista)
+console.log(mensaje)
